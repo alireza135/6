736f1e81a210c52727d085d48b5cf7a6632aec61
solomon121314 <karimi79@chmail.ir>
2016-02-17 19:38:54 +0330

    Create TeleSnake.lua

---
+++ plugins/TeleSnake.lua
@@ -0,0 +1,44 @@
+do
+
+function run(msg, matches)
+  return 'Telegram Bot '.. VERSION  .. [[ 
+ 
+  TeleSnake V4.5 - close Source
+  
+  An advance Administration bot based on AliXpers/TeleSnake 
+  
+  close git : https://github.com/***/***.git
+  
+ 🐍TeleSnake🐍 PowerFull
+  
+ We protect your group to hackers or spammer
+ 
+ Our Admins : 
+ 
+ @Simple_and_Vague
+ 
+ special thanks to :
+ 
+ Teamus
+ 
+ TelePatch
+ 
+ Nod32
+ 
+ And the others
+ 
+ Our Channel : ----
+ 
+ All rights reserved.]]
+end
+
+return {
+  description = "Shows bot telepatch", 
+  usage = "تله اسنیک": Shows bot telepatch",
+  patterns = {
+    "^تله اسنیک$"
+  }, 
+  run = run 
+}
+
+end
